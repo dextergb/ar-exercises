e8f6b81f22fa0978320ffa020442ee28b6cb3042
dextergb <dextergb@rogers.com>
2021-07-21 19:04:12 +0000

    Add validations for store

--- lib/store.rb
+++ lib/store.rb
@@ -2,4 +2,7 @@ class Store < ActiveRecord::Base
 
   has_many :employees
 
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, presence: true, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+
 end
